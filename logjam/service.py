094ef02e6214da03a75429526822c7e25f447660
Hunter Blanks <hunter@napofearth.com>
2013-09-24 14:04:09 -0700

    logjam/service.py: fix sleep_time calculation in do_forever()

--- logjam/service.py
+++ logjam/service.py
@@ -16,9 +16,11 @@ def do_forever(do_func, interval_secs, *args, **kwargs):
     assumed to be a low-resource agent) never ends up with a rapidly
     iterating loop.
     """
-    last_run = time.time()
     while True:
+        start = time.time()
         do_func(*args, **kwargs)
-        elapsed_time = time.time() - last_run
-        sleep_time = max(interval_secs - elapsed_time, 1)
+        elapsed_time = time.time() - start
+
+        sleep_time = max(interval_secs - elapsed_time, MIN_SLEEP_TIME)
+        logging.debug('service.do_forever: sleeping %.2f', sleep_time)
         time.sleep(sleep_time)
